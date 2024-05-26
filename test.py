d0f0575756e6cf53773ab3e7aba30e03f0f35687
Nickeldon <50726216+Nickeldon@users.noreply.github.com>
2024-05-25 20:29:21 -0400

    version 2

--- test.py
+++ test.py
@@ -1,117 +1,234 @@
 import numpy as np
-import scipy.integrate as integrate
 import matplotlib.pyplot as plt
+import time
+import os
+import math
 
-# Constants
-mu_0 = 4 * np.pi * 1e-7  # Vacuum permeability (H/m)
-current = 75  # Current in the solenoid (A)
-L = 0.5  # Length of the solenoid in meters
-tolerance = 0.05  # 5% tolerance
+used_mem_lib = True
 
-class Mag_Field_equations:
-    def __init__(self, L, I, mu_0):
-        self.L = L 
-        self.I = I
-        self.mew_0 = mu_0
+try:
+    import psutil
+    import threading
+except:
+    used_mem_lib = False
+    print("""
+    ###                                                                            ###
+    ### Please use the psutil and threading modules to show the memory consumption ###
+    ###                                                                            ###""")
+
+start_time = 0
+class setInterval: #I use JS and like the setInterval function
+    def __init__(self, func, ms):
+        self.func = func
+        self.ms = ms
+        self.thread = threading.Timer(self.ms / 1000, self.func_wrapper)
+        
+    def func_wrapper(self):
+        setInterval(self.func, self.ms).init()
+        self.func()
     
-    def _get_B_center(self, turns):
-        n = turns / self.L  # We calculate the coil density (number of turns / length of solenoid)
-        B_center = self.mew_0 * n * self.I
-        return B_center
+    def init(self):    
+        # If I don't use another thread, it will just stay stuck in this function as it will loop indefenitely
+        self.thread.start()
     
-    def _get_mag_field_z(self, z, turns):
-        n = turns / L  # We calculate the coil density (number of turns / length of solenoid)
-        return self.mew_0 * n * self.I * (1 - z**2 / self.L**2)
+    def abort(self):
+        self.thread.cancel()
+        
+
+# Calculate the memory consumption in a different interval that iterates more slowly for more performance
+mem_consump = 0
+thread_interv = None
+
+#Source from Charrat Mohamed's Projectile Motion Assignment
+#Function to calculate the memory used by the simulation
+def get_Mem_Usage():
+    global mem_consump #According to Stack Overflow [To update a global variable in a local scoped function]
+    pr = psutil.Process(os.getpid())
     
-    def _get_avg_mag_field(self, turns):
-        z_pts = np.linspace(-self.L/2, self.L/2, 1000)
-        B_pts = [B_z(z, turns) for z in z_pts]
-        return np.mean(B_pts)
+    # According to https://pythonhow.com/how/limit-floats-to-two-decimal-points/ [To limit floats to two decimal points]
+    
+    mem_consump = round(pr.memory_info()[0] / float(2 ** 20), 2)
+    return mem_consump
 
+if used_mem_lib:
+    thread_interv = setInterval(get_Mem_Usage, 1000)
+    thread_interv.init()
 
-# Function to calculate the magnetic field at the center of a solenoid using Biot-Savart law
-def B_center(N):
-    n = N / L  # Coil density
-    B_c = mu_0 * n * current
-    return B_c
+# Constants for loading bar
+bar_len = 50
+update_interval = 100
 
-# Function to calculate the magnetic field at a point z inside the solenoid using Biot-Savart law
-def B_z(z, N):
-    n = N / L  # Coil density
-    B = mu_0 * n * current * (1 - z**2 / L**2)
-    return B
+#The percentage tolerance for the ideal average Magnetic field value
+tol_percent = 0.075
 
-# Function to calculate the average magnetic field in the interior region of the solenoid
-def B_avg(N):
-    z_points = np.linspace(-L/2, L/2, 1000)
-    B_points = [B_z(z, N) for z in z_points]
-    return np.mean(B_points)
+B_avg_N = []
 
-# Function to check if the magnetic field is within tolerance
-def is_within_tolerance(N):
-    B_c = B_center(N)
-    B_avg_val = B_avg(N)
-    return abs(B_avg_val - B_c) / B_c <= tolerance
-
-# Golden section search for a single variable
-def golden_section_search(f, a, b, tol=1e-5):
-    gr = (np.sqrt(5) + 1) / 2  # Golden ratio
-    c = b - (b - a) / gr
-    d = a + (b - a) / gr
-
-    while abs(b - a) > tol:
-        if f(c) < f(d):
-            b = d
+class Timer:
+    def __init__(self):
+        self.t_0 = 0
+        self.started = False
+        
+    def start_timer(self):
+        self.t_0 = time.time()
+        self.started = True
+    
+    def get_timer(self):
+        elapsed = time.time() - self.t_0
+        if self.started:
+            return int(elapsed)
         else:
-            a = c
+            for _ in range(0, 10):
+                print('The Timer has not been started!', end='\r')
 
-        c = b - (b - a) / gr
-        d = a + (b - a) / gr
+timer = Timer()
 
-    return (b + a) / 2
+timer.start_timer()
 
-# Objective function for the number of turns optimization
-def objective_N(N):
-    B_avg_val = B_avg(N)
-    B_c = B_center(N)
+def print_progress(iter, max_iter, head, asyncIter = ['', '']):
+    percent = int(100 * (iter / int(max_iter)))
+    blocks = int((bar_len * iter / int(max_iter)))
+    bar = '█' * blocks + '-' * (bar_len - blocks)
+    asyncIter_arr = asyncIter
+        
+    print(f'{head} |{bar}| {percent}% ({iter}/{max_iter}) | ({asyncIter_arr[0]} / {asyncIter_arr[1]}) | Mem usage: {mem_consump} MB | elapsed_time: {timer.get_timer()}s  ', end='\r\r')
+    print('\r', end='\r')
+    if iter == max_iter:
+        print('\r', end='\r')
+
+class Solenoid:
+    def __init__(self, I, L):
+        self.I = I
+        self.L = L
+        self.mu_0 = 4 * np.pi * 1e-7
+        self.tol = tol_percent
+        self.dl = 1  # Small length element for integration
+        self.B_0= 0
+        self.B_avg_cache = {}
+
+    def _get_B_center(self, N):
+        n = N / self.L
+        B_cent = self.mu_0 * n * self.I
+        return B_cent
+
+    def _get_B_z(self, z, N):
+            
+        n = N / self.L  # Coil density
+        for s in np.arange(-self.L/2, self.L/2, self.dl):
+            r = np.sqrt(z**2 + (s - z)**2)
+            dB = (self.mu_0 * n * self.I * self.dl) / (2 * np.pi * r)
+            self.B_0 += dB
+        return self.B_0
+
+    def _get_avg_B(self, N, iterations = None, showProg = False, skip_bar=False):
+        if N in self.B_avg_cache:  # Check if the result is already in the cache
+            return self.B_avg_cache[N]
+
+        z_pts = np.linspace(-self.L / 2, self.L / 2, 100000)
+        B_pts = []
+        for i, val in enumerate(z_pts):
+            B_pts.append(self._get_B_z(val, N))
+            if not skip_bar and (i % update_interval == 0 or i == len(z_pts) - 1):
+                if not iterations:
+                    iter_arr = ('', '')
+                else:
+                    iter_arr = iterations
+                print_progress(i + 1, len(z_pts), head='Calculating B:', asyncIter = iter_arr)
+        B_avg = np.mean(B_pts)
+        self.B_avg_cache[N] = B_avg  # Store the result in the cache
+        return B_avg
+
+    def _verif_if_within_tol(self, N):
+        print(f'Initializing simulation... ** Note: The initialization will take a long time if the iteration value is high **', end='\r')
+        B_cent = self._get_B_center(N)
+        B_avg = self._get_avg_B(N, skip_bar=True)
+        return abs(B_avg - B_cent) / B_cent <= self.tol
+
+class GoldenSectionSearch:
+    def __init__(self, f, a, b, tol=1e-5):
+        self.f = f
+        self.a = a
+        self.b = b
+        self.tol = tol
+        self.gr = (np.sqrt(5) + 1) / 2
+    
+    def estim_max_iter(self):
+        return math.ceil(math.log(self.tol/abs(self.b-self.a)) / math.log(1/self.gr))
+
+    def search(self):
+        es_max = self.estim_max_iter() # Estimated maximum number of iterations through the golden section search
+        c = self.b - (self.b - self.a) / self.gr
+        d = self.a + (self.b - self.a) / self.gr
+        i = 0
+        while abs(self.b - self.a) > self.tol:
+            i += 1
+            if self.f(c, (i, es_max)) < self.f(d, (i, es_max)):
+                self.b = d
+            else:
+                self.a = c
+
+            c = self.b - (self.b - self.a) / self.gr
+            d = self.a + (self.b - self.a) / self.gr
+
+        return (self.b + self.a) / 2, i
+
+# STARTING CONSTANTS
+I = 75  # Current in the solenoid (Will not change as is irrelevant to experiment) (A)
+L = 0.5  # Length of the solenoid (m)
+
+# Create Solenoid instance
+solenoid = Solenoid(I, L)
+
+def objective_N(N, iter_state):
+    B_c = solenoid._get_B_center(N)
+    B_avg_val = solenoid._get_avg_B(N, iterations = iter_state, skip_bar=False, showProg=True)
+    B_avg_N.append(B_avg_val)
     return abs(B_avg_val - B_c) / B_c
 
-# Function to dynamically determine the bounds for the golden section search
 def find_initial_bounds():
     N = 1
-    while not is_within_tolerance(N):
+    while not solenoid._verif_if_within_tol(N):
         N *= 2
-        if N > 1e6:  # Preventing too large numbers
+        if N > 1e5:  # If N becomes too large, break
             break
-    lower_bound = N // 2 if N < 1e6 else N // 4
-    upper_bound = N if N < 1e6 else N // 2
-    return lower_bound, upper_bound
+    min = N // 2 if N < 1e5 else N // 4
+    max = N if N < 1e5 else N // 2
+    return min, max
 
-# Find the initial bounds
+# Clear console
+os.system('cls')
+
 a_N, b_N = find_initial_bounds()
 
+gss = GoldenSectionSearch(objective_N, a_N, b_N)
+
 # Optimize for the minimum number of turns
-optimal_N = golden_section_search(objective_N, a_N, b_N)
+optimal_N, i = gss.search()
 
 # Print the optimal number of coils
-print(f"The optimal number of coils (N) for a uniform magnetic field within 5% tolerance is: {optimal_N}")
+print(f"The optimal number of coils (N) for a uniform magnetic field within {tol_percent * 100}% tolerance is: {optimal_N}")
 
 # Calculate the magnetic fields for plotting
-N_values = np.linspace(a_N, b_N, 500)
-B_avg_values = [B_avg(N) for N in N_values]
-B_center_values = [B_center(N) for N in N_values]
+N_arr = np.linspace(a_N, b_N, 2 * i)
+B_avg_arr = []
+print('The graph is being drawn. Please wait')
+for i, N in enumerate(N_arr):
+    B_avg_arr.append(solenoid._get_avg_B(N, showProg = True, skip_bar=True))
+    print_progress(i + 1, len(N_arr), head='Calculating B_avg for plot:')
+B_cent_arr = [solenoid._get_B_center(N) for N in N_arr]
 
 # Calculate the 5% tolerance bands
-B_center_optimal = B_center(optimal_N)
-tolerance_band_upper = B_center_optimal * (1 + tolerance)
-tolerance_band_lower = B_center_optimal * (1 - tolerance)
+B_mid_opt = solenoid._get_B_center(optimal_N)
+tol_band_upper = B_mid_opt * (1 + solenoid.tol)
+tol_band_lower = B_mid_opt * (1 - solenoid.tol)
+
+if used_mem_lib:
+    thread_interv.abort()
 
 # Plot the results
 plt.figure(figsize=(10, 6))
-plt.plot(N_values, B_avg_values, label='Average Magnetic Field in Solenoid')
-plt.plot(N_values, B_center_values, label='Magnetic Field at Center')
-plt.axhline(y=tolerance_band_upper, color='r', linestyle='--', label='5% Tolerance Band')
-plt.axhline(y=tolerance_band_lower, color='r', linestyle='--')
+plt.plot(N_arr, B_cent_arr, label='Magnetic Field at Center')
+plt.axhline(y=tol_band_upper, color='r', linestyle='--', label=f'{tol_percent * 100}% tolerance Band')
+plt.axhline(y=tol_band_lower, color='r', linestyle='--')
 plt.axvline(x=optimal_N, color='g', linestyle='--', label='Optimal Number of Turns')
 plt.xlabel('Number of Turns (N)')
 plt.ylabel('Magnetic Field (T)')
